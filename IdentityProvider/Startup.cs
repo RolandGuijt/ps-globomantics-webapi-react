428f5ed8e4e8de45c979cb54b47a72bec224f86f
Roland Guijt <roland.guijt@gmail.com>
2024-02-29 11:35:43 +0100

    Remove IdentityServer version number

--- IdentityProvider/Startup.cs
+++ IdentityProvider/Startup.cs
@@ -33,8 +33,6 @@ namespace Part2_TokenService
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
-
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
                 
             })
